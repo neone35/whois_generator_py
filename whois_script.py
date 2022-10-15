070ce52db56d2bed6f4549fa7a56bd3a604a45ac
Artur Maslov <aartasas@gmail.com>
2022-10-15 21:34:40 +0300

    Resolve expiration date parsing with datetime/list recognition

--- whois_script.py
+++ whois_script.py
@@ -9,29 +9,33 @@ data = pd.read_csv("domain_export.csv")
 
 domain_names = []
 exp_dates = []
-is_datetimes = []
+type_of_date = []
 for name in data.name:
     domain_names.append(name)
     w = whois.whois(name)
-    time.sleep(2)
     exp_date = w.expiration_date  # dates converted to datetime object
+    print(exp_date)
     if isinstance(exp_date, datetime):
         exp_date = exp_date.strftime("%Y-%m-%d %H:%M:%S") # format unformatted
-        is_datetimes.append('yes')
+        type_of_date.append('datetime')
+    elif isinstance(exp_date, list):
+        print(exp_date)
+        exp_date = exp_date[0].strftime("%Y-%m-%d %H:%M:%S") # format unformatted
+        type_of_date.append('list')
     else:
-        is_datetimes.append('no')
+        type_of_date.append('none')
     exp_dates.append(exp_date)
 
 data_arr = []
 for i in range(0, len(data)):
-    data_arr.insert(i, [domain_names[i], exp_dates[i], is_datetimes[i]])
+    data_arr.insert(i, [domain_names[i], exp_dates[i], type_of_date[i]])
 
 f = open("output.txt", "w")
 for i in range(0, len(data_arr)):
     f.write(f"{data_arr[i][0]} {data_arr[i][1]} {data_arr[i][2]}\n")
 f.close()
 
-out_csv_header = ['Domain', 'Expiration date', 'Datetime?']
+out_csv_header = ['Domain', 'Expiration date', 'Date type']
 pd_data = pd.DataFrame(data_arr, columns=out_csv_header)
 pd_data.to_csv('output.csv', index=False)
 
